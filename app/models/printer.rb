e838857831647e2aaa3d6c5741fb1aa1bda8bf5a
Bob Shannon <rshannon@buffalo.edu>
2016-02-22 23:10:37 +0000

    Add more fields to Printer

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -1,4 +1,5 @@
 class Printer < ApplicationRecord
   has_one :hub_printer
   has_one :hub, through: :hub_printer
+  has_many :jobs, through: :printer_jobs
 end
--- db/migrate/20160222214812_create_printers.rb
+++ db/migrate/20160222214812_create_printers.rb
@@ -2,6 +2,9 @@ class CreatePrinters < ActiveRecord::Migration[5.0]
   def change
     create_table :printers do |t|
       t.string :label
+      t.string :manufacturer
+      t.string :model
+      t.string :status
       t.timestamps
     end
   end
