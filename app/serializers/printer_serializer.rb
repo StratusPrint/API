fdaafef21868dc9014acc9a4b2fde847bca619f5
Bob Shannon <rshannon@buffalo.edu>
2016-02-25 05:17:37 +0000

    More serialization

--- app/serializers/printer_serializer.rb
+++ app/serializers/printer_serializer.rb
@@ -1,3 +1,7 @@
 class PrinterSerializer < ActiveModel::Serializer
-  attributes :id, :friendly_id, :label, :manufacturer, :model, :status
+  attributes :id, :friendly_id, :label, :manufacturer, :model, :status, :jobs
+
+  def jobs
+    object.jobs.as_json
+  end
 end
