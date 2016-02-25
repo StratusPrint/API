9afbdd4fd3788b2c126ee8649ed3173f840abbdb
Bob Shannon <rshannon@buffalo.edu>
2016-02-25 07:48:33 +0000

    Update sensor serializer to ignore updated_at attribute

--- app/serializers/sensor_serializer.rb
+++ app/serializers/sensor_serializer.rb
@@ -2,6 +2,6 @@ class SensorSerializer < ActiveModel::Serializer
   attributes :id, :friendly_id, :category, :manufacturer, :model, :desc, :data
 
   def data
-    object.data_points.as_json(except: [:id, :update_at])
+    object.data_points.as_json(except: [:id, :updated_at])
   end
 end
