4a52ed81ce8aa529819cba0084505d6818e1339b
Bob Shannon <rshannon@buffalo.edu>
2016-02-28 19:56:45 +0000

    Remove before_create hook; API key generation now handled by devise_token_auth

--- app/models/hub.rb
+++ app/models/hub.rb
@@ -1,5 +1,4 @@
 class Hub < ApplicationRecord
-  before_create :gen_api_key
   validates :friendly_id, :uniqueness => true
 
   has_many :hub_printers
@@ -10,13 +9,4 @@ class Hub < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :trackable
   include DeviseTokenAuth::Concerns::User
-
-  private
-
-  def gen_api_key
-    begin
-      self.api_token = SecureRandom.hex
-      self.uid = api_token
-    end while self.class.exists?(api_token: api_token)
-  end
 end
