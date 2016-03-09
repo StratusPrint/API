6107ed6eb018d7a7fced50e7437481f5dec168da
Bob Shannon <rshannon@buffalo.edu>
2016-03-09 00:42:58 +0000

    Condense include statements

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,12 +2,9 @@ module Api::V1
   class ApiController < ApplicationController
     # Provide controllers access to methods such as
     # authenticate_user!, user_signed_in?, swagger_path, etc
-    include DeviseTokenAuth::Concerns::SetUserByToken
-    include CanCan::ControllerAdditions
-    include Swagger::Blocks
+    include Swagger::Blocks, CanCan::ControllerAdditions, DeviseTokenAuth::Concerns::SetUserByToken
 
-    # Authenticate user or hub before allowing them to use the
-    # API
+    # Authenticate user or hub before allowing them to use the API
     devise_token_auth_group :hub_or_user, contains: [:hub, :user]
     before_action :authenticate_hub_or_user!
 
