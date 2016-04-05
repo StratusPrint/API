4688d7881d368f943214fa2dbfb9735541c346b0
Bob Shannon <rshannon@buffalo.edu>
2016-04-05 05:40:33 +0000

    Whitelist additional user model parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  # "Whitelist" additional user parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
