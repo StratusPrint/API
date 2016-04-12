a4e5d8ce3e38ab5be4ea7ca63ed995c5e64ab953
Bob Shannon <rshannon@buffalo.edu>
2016-04-12 06:41:31 +0000

    Sanitize additional user parameters on sign up. This fixes the issue where the name is not being set upon registration.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :image, :admin])
   end
 end
