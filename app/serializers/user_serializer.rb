98f8508b5f102a04d2127b50c38255805858d7cb
Bob Shannon <rshannon@buffalo.edu>
2016-04-12 06:45:35 +0000

    Expose users' last_sign_in_ip, last_sign_in_at, and created_at

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :email, :admin
+  attributes :id, :name, :image, :email, :admin, :last_sign_in_ip, :last_sign_in_at, :created_at
 end
