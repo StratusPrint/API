7b88734a99462acba4df6981a40e178c180f0413
Bob Shannon <rshannon@buffalo.edu>
2016-04-12 23:25:47 +0000

    More user management tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,12 +3,19 @@ FactoryGirl.define do
     "testuser#{n}@stratusprint.com"
   end
 
+  sequence :user_ip do |n|
+    IPAddr.new(n, Socket::AF_INET).to_s
+  end
+
   factory :user do
     name "John Smith"
     nickname "John"
     email { generate(:email) }
     password "12345678"
     admin false
+    last_sign_in_ip { generate(:ip) }
+    last_sign_in_at Time.now
+    created_at Time.now
   end
 
   factory :admin, class: User do
@@ -17,5 +24,8 @@ FactoryGirl.define do
     email "admin@stratusprint.com"
     password "12345678"
     admin true
+    last_sign_in_ip { generate(:user_ip) }
+    last_sign_in_at Time.now
+    created_at Time.now
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,6 +4,7 @@ describe "User Management", :type => :request do
   let!(:users) { create_list(:user, 5) }
   let(:admin) { create :admin }
   let(:user) { create :user }
+  let(:new_user) { build :user }
   let(:admin_auth_headers) { admin.create_new_auth_token }
   let(:user_auth_headers) { user.create_new_auth_token }
 
@@ -32,10 +33,9 @@ describe "User Management", :type => :request do
       expect(response).to have_http_status(:unauthorized)
     end
 
-    it "should return a user if authenticated as that user" do
+    it "should not return a user if authenticated as that user" do
       get v1_user_path(user.id), headers: user_auth_headers
-      expect(response).to have_http_status(:success)
-      expect(response).to match_response_schema("user")
+      expect(response).to have_http_status(:forbidden)
     end
 
     it "should not return a user if not authenticated as that user" do
@@ -47,6 +47,50 @@ describe "User Management", :type => :request do
       get v1_user_path(user.id), headers: admin_auth_headers
       expect(response).to have_http_status(:success)
       expect(response).to match_response_schema("user")
+    end
+  end
+
+  context "DELETE /users/:id" do
+    it "should not delete a user if not authenticated" do
+      delete v1_user_path(user.id)
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it "should not delete a user if authenticated as that user" do
+      delete v1_user_path(user.id), headers: user_auth_headers
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "should not delete a user if not authenticated as that user" do
+      delete v1_user_path(users.first.id), headers: user_auth_headers
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "should delete a user if authenticated as admin" do
+      delete v1_user_path(user.id), headers: admin_auth_headers
+      expect(response).to have_http_status(:no_content)
+    end
+  end
+
+  context "PATCH /users/:id" do
+    it "should not update a user if not authenticated" do
+      patch v1_user_path(user.id), params: { user: new_user.attributes }
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it "should not update a user if authenticated as that user" do
+      patch v1_user_path(user.id), headers: user_auth_headers, params: { user: new_user.attributes }
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "should not update a user if not authenticated as that user" do
+      patch v1_user_path(users.first.id), headers: user_auth_headers, params: { user: new_user.attributes }
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "should update a user if authenticated as admin" do
+      patch v1_user_path(user.id), headers: admin_auth_headers, params: { user: new_user.attributes }
+      expect(response).to have_http_status(:success)
     end
   end
 end
