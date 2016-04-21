49311bdedf95d161338b1f4a0915d35f45d99840
Bob Shannon <rshannon@buffalo.edu>
2016-04-21 04:07:00 +0000

    Add more model file name fields to job response; fix migration
    model_file_name (e.g. gear)
    model_file_extension (e.g. stl)
    model_fil_name_full (e.g. gear.stl)

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,7 +1,21 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :updated_at, :model_file_url, :data
+  attributes :id, :created_at, :updated_at, :model_file_url, :model_file_name, :model_file_extension, :model_file_name_full, :data
 end
 
 def model_file_url
   self.model.url
+end
+
+def model_file_name
+  self.model_file_name
+end
+
+def model_file_extension
+  "test"
+end
+
+def model_file_name_full
+  name = self.model_file_name
+  extension = model_file_extension
+  "#{name}.#{extension}"
 end
--- db/migrate/20160421033004_add_model_file_name_to_jobs.rb
+++ db/migrate/20160421033004_add_model_file_name_to_jobs.rb
@@ -1,4 +1,4 @@
-class AddModelNameToJobs < ActiveRecord::Migration[5.0]
+class AddModelFileNameToJobs < ActiveRecord::Migration[5.0]
   def change
     add_column :jobs, :model_file_name, :text
   end
