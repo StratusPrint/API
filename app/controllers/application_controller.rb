25781a6347b0f4f5275900c3f33e19a8cdf6af2a
Bob Shannon <rshannon@buffalo.edu>
2016-04-24 21:51:45 +0000

    Allow default_hub_id to be set on sign up and account update endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :image])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :image, :admin])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :image, :default_hub_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :image, :admin, :default_hub_id])
   end
 end
