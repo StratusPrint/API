6a5d5996df4907fb310d7fa739aaff173d85a997
Bob Shannon <rshannon@buffalo.edu>
2016-04-25 18:51:21 +0000

    Set command to errored if unable to send to hub

--- app/jobs/send_printer_command_job.rb
+++ app/jobs/send_printer_command_job.rb
@@ -6,7 +6,7 @@ class SendPrinterCommandJob < ApplicationJob
     @command = command
 
     begin
-      RestClient.post(hub_endpoint) { |response, request, result, &block|
+      RestClient.post(hub_endpoint, :command_id => @command.id) { |response, request, result, &block|
         case response.code
         when 201
           logger.info "#{command} command successfully sent to hub ##{@hub.id} for printer ##{printer.id}."
@@ -14,12 +14,18 @@ class SendPrinterCommandJob < ApplicationJob
         end
       }
     rescue
-      logger.info "Unable to send #{command} command to hub ##{@hub.id} for printer ##{printer.id}."
+      set_job_errored
     end
   end
 
   private
   def hub_endpoint
     "http://#{@hub.ip}/#{@hub.id}/printers/#{@printer.id}/#{@command}"
+  end
+
+  def set_command_errored
+    logger.info "Unable to send #{command} command to hub ##{@hub.id} for printer ##{printer.id}."
+    @command.status = 'errored'
+    @command.save
   end
 end
