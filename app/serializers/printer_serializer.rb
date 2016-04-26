2996ca1c53744c5c3d1e7c0d31bb7e2687c043bf
Bob Shannon <rshannon@buffalo.edu>
2016-04-26 21:41:42 +0000

    Update printer model serializer

--- app/serializers/printer_serializer.rb
+++ app/serializers/printer_serializer.rb
@@ -1,5 +1,5 @@
 class PrinterSerializer < ActiveModel::Serializer
-  attributes :id, :updated_at, :friendly_id, :manufacturer, :model, :num_jobs, :description, :data
+  attributes :id, :updated_at, :friendly_id, :manufacturer, :model, :num_jobs, :description, :status
 
   def num_jobs
     object.jobs.count
