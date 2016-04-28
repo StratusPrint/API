0aa0328215264a125d2bfd0e3b8c6776ef6e54b7
Bob Shannon <rshannon@buffalo.edu>
2016-04-28 15:49:27 +0000

    Tweak

--- app/jobs/send_printer_command_job.rb
+++ app/jobs/send_printer_command_job.rb
@@ -4,12 +4,13 @@ class SendPrinterCommandJob < ApplicationJob
   def perform(command, printer)
     @hub = printer.hub
     @command = command
+    @printer = printer
 
     begin
       RestClient.post(hub_endpoint, :command_id => @command.id) { |response, request, result, &block|
         case response.code
         when 201
-          logger.info "#{command} command successfully sent to hub ##{@hub.id} for printer ##{printer.id}."
+          logger.info "#{@command} command successfully sent to hub ##{@hub.id} for printer ##{@printer.id}."
           logger.debug "Response from hub: " + response
         end
       }
@@ -24,7 +25,7 @@ class SendPrinterCommandJob < ApplicationJob
   end
 
   def set_command_errored
-    logger.info "Unable to send #{command} command to hub ##{@hub.id} for printer ##{printer.id}."
+    logger.info "Unable to send #{@command} command to hub ##{@hub.id} for printer ##{@printer.id}."
     @command.status = 'errored'
     @command.save
   end
