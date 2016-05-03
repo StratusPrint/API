7b9ace7279822ecab302c8d6d2b18af6107f4ab0
Bobby Shannon <rshannon@buffalo.edu>
2016-05-03 16:03:55 -0400

    Catch exception

--- app/jobs/register_sensor_job.rb
+++ app/jobs/register_sensor_job.rb
@@ -14,14 +14,18 @@ class RegisterSensorJob < ApplicationJob
   end
 
   def register_sensor
-    RestClient.post(hub_endpoint, {:id => @sensor.id, :pin => @sensor.pin, :sensor_type => @sensor.category}.to_json, :content_type => :json, :accept => :json) { |response, request, result, &block|
-      case response.code
-      when 201
-        logger.application.info "Sensor ##{@sensor.id} (#{@sensor.name}) successfully registered with hub ##{@hub.id}."
-        logger.application.debug "Response from hub: " + response
-      else
-        logger.application.info "Unable to register ##{@sensor.id} (#{@sensor.name}) with hub ##{@hub.id}."
-      end
-    }
+    begin
+      RestClient.post(hub_endpoint, {:id => @sensor.id, :pin => @sensor.pin, :sensor_type => @sensor.category}.to_json, :content_type => :json, :accept => :json) { |response, request, result, &block|
+        case response.code
+        when 201
+          logger.application.info "Sensor ##{@sensor.id} (#{@sensor.name}) successfully registered with hub ##{@hub.id}."
+          logger.application.debug "Response from hub: " + response
+        else
+          logger.application.info "Unable to register ##{@sensor.id} (#{@sensor.name}) with hub ##{@hub.id}."
+        end
+      }
+    rescue
+      # Do nothing
+    end
   end
 end
